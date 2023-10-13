6d4957b456fa8bb2dcc157557000ba6044bdb72e
Jan Pfeifer <jan.pfeifer@uni-wuppertal.de>
2023-10-13 12:49:43 +0200

    [pip] Add some more useful modules

--- installs/pip.sh
+++ installs/pip.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # python installs - DO NOT RUN AS ROOT!
-python3 -m pip install -U pip numpy scipy sympy matplotlib
+python3 -m pip install -U pip numpy scipy sympy matplotlib numba
 python3 -m pip install -U pip virtualenv
-python3 -m pip install -U pip black isort mypy
+python3 -m pip install -U pip black isort mypy pyright ruff
 python3 -m pip install -U pip fortls debugpy
 python3 -m pip install -U pip gmsh
 python3 -m pip install -U pip thefuck
 python3 -m pip install -U pip vtk
-python3 -m pip install -U pip pyvista
+python3 -m pip install -U pip pyvista imageio[ffmpeg] imageio[pyav]
 python3 -m pip install -U pip jupyterlab
 python3 -m pip install -U pip notebook
 python3 -m pip install -U pip jupyter-book
@@ -17,9 +17,12 @@ python3 -m pip install -U pip cpplint
 python3 -m pip install -U pip scholarly
 python3 -m pip install -U pip openpyxl
 python3 -m pip install -U pip pandas
-python3 -m pip install -U pip polars
+python3 -m pip install -U pip polars[all]
 python3 -m pip install -U pip meshio[all]
 python3 -m pip install -U pip gstools
 python3 -m pip install -U pip snakeviz
 python3 -m pip install -U pip torch torchvision torchaudio
 python3 -m pip install -U pip python-frontmatter
+python3 -m pip install -U pip beautifulsoup4
+python3 -m pip install -U pip icecream
+python3 -m pip install -U pip h5py
