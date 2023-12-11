80127ad32a7d0d1fc1ea83d42fb0a2e2e686dbcd
Jan Pfeifer <jan.pfeifer@uni-wuppertal.de>
2023-12-12 00:23:07 +0100

    [pip] Switch -U -> --upgrade for readability

--- installs/pip.sh
+++ installs/pip.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # python installs - DO NOT RUN AS ROOT!
-python3 -m pip install -U pip numpy scipy sympy matplotlib numba
-python3 -m pip install -U pip virtualenv setuptools wheel build
-python3 -m pip install -U pip black isort mypy pyright ruff pytest
-python3 -m pip install -U pip clang-format clang-tidy
-python3 -m pip install -U pip fortls debugpy
-python3 -m pip install -U pip gmsh
-python3 -m pip install -U pip thefuck
-python3 -m pip install -U pip vtk
-python3 -m pip install -U pip pyvista imageio[ffmpeg] imageio[pyav]
-python3 -m pip install -U pip jupyterlab
-python3 -m pip install -U pip notebook
-python3 -m pip install -U pip jupyter-book
-python3 -m pip install -U pip jinja2
-python3 -m pip install -U pip cpplint
-python3 -m pip install -U pip scholarly
-python3 -m pip install -U pip openpyxl
-python3 -m pip install -U pip pandas
-python3 -m pip install -U pip polars[all]
-python3 -m pip install -U pip meshio[all]
-python3 -m pip install -U pip gstools
-python3 -m pip install -U pip snakeviz
-python3 -m pip install -U pip torch torchvision torchaudio
-python3 -m pip install -U pip python-frontmatter
-python3 -m pip install -U pip beautifulsoup4
-python3 -m pip install -U pip icecream
-python3 -m pip install -U pip h5py
-python3 -m pip install -U pip yt-dlp
-python3 -m pip install -U pip tldr
-python3 -m pip install -U pip abqpy
+python3 -m pip install --upgrade pip numpy scipy sympy matplotlib numba
+python3 -m pip install --upgrade pip virtualenv setuptools wheel build
+python3 -m pip install --upgrade pip black isort mypy pyright ruff pytest
+python3 -m pip install --upgrade pip clang-format clang-tidy
+python3 -m pip install --upgrade pip fortls debugpy
+python3 -m pip install --upgrade pip gmsh
+python3 -m pip install --upgrade pip thefuck
+python3 -m pip install --upgrade pip vtk
+python3 -m pip install --upgrade pip pyvista imageio[ffmpeg] imageio[pyav]
+python3 -m pip install --upgrade pip jupyterlab
+python3 -m pip install --upgrade pip notebook
+python3 -m pip install --upgrade pip jupyter-book
+python3 -m pip install --upgrade pip jinja2
+python3 -m pip install --upgrade pip cpplint
+python3 -m pip install --upgrade pip scholarly
+python3 -m pip install --upgrade pip openpyxl
+python3 -m pip install --upgrade pip pandas
+python3 -m pip install --upgrade pip polars[all]
+python3 -m pip install --upgrade pip meshio[all]
+python3 -m pip install --upgrade pip gstools
+python3 -m pip install --upgrade pip snakeviz
+python3 -m pip install --upgrade pip torch torchvision torchaudio
+python3 -m pip install --upgrade pip python-frontmatter
+python3 -m pip install --upgrade pip beautifulsoup4
+python3 -m pip install --upgrade pip icecream
+python3 -m pip install --upgrade pip h5py
+python3 -m pip install --upgrade pip yt-dlp
+python3 -m pip install --upgrade pip tldr
+python3 -m pip install --upgrade pip abqpy
