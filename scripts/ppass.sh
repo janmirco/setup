475925f023eed9ff15c4417a99a1a90b7df7c5fe
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2024-02-16 15:39:55 +0100

    [scripts/ppass] Add ascii header

--- scripts/ppass.sh
+++ scripts/ppass.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo '    ____  ____  ____ ___________'
+echo '   / __ \/ __ \/ __ `/ ___/ ___/'
+echo '  / /_/ / /_/ / /_/ (__  |__  )'
+echo ' / .___/ .___/\__,_/____/____/'
+echo '/_/   /_/'
+echo
+
 cd $HOME/.password-store
 fdfind --extension gpg . . | sed "s/\.\///" | sed "s/\.gpg//" | fzf --height=10% | xargs pass | xclip -rmlastnl
 echo "Clipboard will be cleared and terminal will be closed in 20 seconds ..."
