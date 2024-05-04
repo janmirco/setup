c2667e8f7cc323453bc5201c879db3d743c1ac8d
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2024-05-04 18:44:47 +0200

    feat(scripts): use fzf to search git commits

---
+++ scripts/fgd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function fzf_git_diff() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort --preview \
+         'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
+      --bind "q:abort,ctrl-f:preview-page-down,ctrl-b:preview-page-up" \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF" --preview-window=right:60%
+}
+
+fzf_git_diff "$@"
