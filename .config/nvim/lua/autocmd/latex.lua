ff16f2fa8f7dc4cdb1626ec1461524b5c9cd6543
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2024-07-06 12:48:06 +0200

    feat(nvim/latex): add spell checking for bib files

--- .config/nvim/lua/autocmd/latex.lua
+++ .config/nvim/lua/autocmd/latex.lua
@@ -1,9 +1,15 @@
 local latex_group = vim.api.nvim_create_augroup("LatexAugroup", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType", "BufEnter", "BufWinEnter", "BufRead" }, {
     group = latex_group,
-    pattern = "*.tex",
+    pattern = { "*.bib", "*.tex" },
     callback = function()
-        vim.cmd("setfiletype tex")
+        if vim.api.nvim_eval("&filetype") == "bib" then
+            vim.cmd("syntax match bibText /{[^}]*}/ contains=@Spell")
+            vim.cmd("syntax match bibText /\".*\"/ contains=@Spell")
+        else
+            vim.cmd("setfiletype tex")
+        end
+
         vim.g.tex_flavor = "latex"
         vim.g.tex_nospell = 0
         vim.g.tex_comment_nospell = 0
