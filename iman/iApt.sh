d1c8427952c4a853dd0befbce5d7a60b43e01132
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2024-10-23 09:13:28 +0200

    feat(iApt): remove sciebo and pulse vpn

--- iman/iApt.sh
+++ iman/iApt.sh
@@ -100,24 +100,5 @@ if ! command -v brave-browser &> /dev/null; then
     sudo apt-get install --assume-yes brave-browser
 fi
 
-# Sciebo
-if ! command -v sciebo &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get install --assume-yes libicu70 libzstd1 apt-transport-https
-    wget -nv https://install.sciebo.de/linux/Ubuntu_22.04/Release.key -O - | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sciebo-client.gpg > /dev/null
-    echo 'deb https://install.sciebo.de/linux/Ubuntu_22.04/ /' | sudo tee -a /etc/apt/sources.list.d/sciebo-client.list
-    sudo apt-get update
-    sudo apt-get install --assume-yes sciebo-client sciebo-client-*
-fi
-
-# Pulse/Ivanti vpn problem, see https://zim.uni-wuppertal.de/de/unsere-dienste/netzzugang/vpn-aussenzugang/pulse-secure/#c135068
-if ! [ -f '/etc/apt/sources.list.d/home:bluca:openconnect.list' ]; then
-    echo 'deb http://download.opensuse.org/repositories/home:/bluca:/openconnect/Ubuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:bluca:openconnect.list
-    curl -fsSL https://download.opensuse.org/repositories/home:bluca:openconnect/Ubuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_bluca_openconnect.gpg > /dev/null
-    sudo apt-get update
-    sudo apt-get install --assume-yes openconnect network-manager-openconnect
-    sudo apt-get install --assume-yes network-manager network-manager-config-connectivity-ubuntu network-manager-dev network-manager-fortisslvpn network-manager-gnome network-manager-iodine network-manager-l2tp network-manager-l2tp-gnome network-manager-openconnect network-manager-openvpn network-manager-pptp network-manager-ssh network-manager-sstp network-manager-strongswan network-manager-vpnc
-fi
-
 # final update and upgrade
 sudo apt-get update && sudo apt-get upgrade --assume-yes
