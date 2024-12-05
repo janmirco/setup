8ffc33c717ecef09f2adb55a5fffb8f7ffd96297
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2024-12-05 08:50:36 +0100

    feat(nvim/markdown): use brave-browser for preview

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -8,7 +8,7 @@ return {
         config = function()
             vim.g.mkdp_auto_close = 0
             vim.g.mkdp_theme = "light"
-            vim.g.mkdp_browser = "brave-browser-stable"
+            vim.g.mkdp_browser = "brave-browser"
             vim.keymap.set("n", "<A-p>", "<Plug>MarkdownPreview", { silent = true })
         end,
     },
