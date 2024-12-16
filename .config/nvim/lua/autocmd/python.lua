8ebd42515eab657af98d52d0f437efbcb54e1dcb
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2024-12-16 07:06:46 +0100

    feat(nvim/autocmd/python): add max line length option

--- .config/nvim/lua/autocmd/python.lua
+++ .config/nvim/lua/autocmd/python.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     group = py_group,
     pattern = "*.py",
     callback = function()
-        vim.cmd(":silent exec '! ruff check --select I --fix %'") -- sort imports
-        vim.cmd(":silent exec '! ruff format %'")
+        vim.cmd(":silent exec '! ruff check --select I --fix --line-length=320 %'") -- sort imports
+        vim.cmd(":silent exec '! ruff format --line-length=320 %'")
     end,
 })
