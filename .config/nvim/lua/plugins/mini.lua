df71f78efd2f2a9fda45eb60c9b46afface9fdba
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2025-01-17 21:55:19 +0100

    feat(nvim): switch from oil and neo-tree to mini.files

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,16 @@
 return {
     { "echasnovski/mini.ai", version = "*", config = function() require("mini.ai").setup() end },
+    {
+        "echasnovski/mini.files",
+        version = "*",
+        config = function()
+            local mini_files = require("mini.files")
+            mini_files.setup()
+            vim.keymap.set("n", "-", function()
+                if not mini_files.close() then mini_files.open() end
+            end, { desc = "mini.files toggle", silent = true })
+        end,
+    },
     { "echasnovski/mini.move", version = "*", config = function() require("mini.move").setup() end },
     {
         "echasnovski/mini.pairs",
--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,26 +1,26 @@
 return {
-    "stevearc/oil.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        local oil = require("oil")
-
-        oil.setup({
-            default_file_explorer = true,
-            skip_confirm_for_simple_edits = true,
-            use_default_keymaps = false,
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<CR>"] = "actions.select",
-                ["q"] = "actions.close",
-                ["-"] = "actions.parent",
-                ["_"] = "actions.open_cwd",
-                ["g."] = "actions.toggle_hidden",
-            },
-            view_options = {
-                show_hidden = true,
-            },
-        })
-
-        vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
-    end,
+    -- "stevearc/oil.nvim",
+    -- dependencies = "nvim-tree/nvim-web-devicons",
+    -- config = function()
+    --     local oil = require("oil")
+    --
+    --     oil.setup({
+    --         default_file_explorer = true,
+    --         skip_confirm_for_simple_edits = true,
+    --         use_default_keymaps = false,
+    --         keymaps = {
+    --             ["g?"] = "actions.show_help",
+    --             ["<CR>"] = "actions.select",
+    --             ["q"] = "actions.close",
+    --             ["-"] = "actions.parent",
+    --             ["_"] = "actions.open_cwd",
+    --             ["g."] = "actions.toggle_hidden",
+    --         },
+    --         view_options = {
+    --             show_hidden = true,
+    --         },
+    --     })
+    --
+    --     vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
+    -- end,
 }
