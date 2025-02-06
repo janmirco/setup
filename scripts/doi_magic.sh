a7ca99e1da7472917c1af660122f93186ccf4d6a
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2025-02-06 22:57:27 +0100

    feat(doi): improve jq and fix sorting for yaml

--- scripts/doi_magic.sh
+++ scripts/doi_magic.sh
@@ -5,15 +5,15 @@ if [[ "$1" == "bib" || "$1" == "bibtex" || "$1" == "tex" ]]; then
 
 elif [[ "$1" == "json" ]]; then
     curl --silent --location --header "Accept: application/citeproc+json" "$2" | jq '{
-    type: "article",
+    type,
     title,
-    volume,
+    volume: (.volume | tonumber),
     ISSN,
-    DOI,
-    number: (.issue // null),
+    DOI: ("https://doi.org/" + .DOI),
+    number: (.issue | tonumber // null),
     journal: ."container-title",
     publisher: (.publisher // ."publisher-name" // .publisher[0].name // null),
-    author: [.author[] | {family, given}],
+    author: [.author[] | "\(.family), \(.given)"],
     year: .issued."date-parts"[0][0],
     month: (.issued."date-parts"[0][1] | tonumber | . - 1 | ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"][.]),
     pages: (.page // null)
@@ -27,18 +27,33 @@ elif [[ "$1" == "txt" || "$1" == "apa" ]]; then
 
 elif [[ "$1" == "yaml" ]]; then
     curl --silent --location --header "Accept: application/citeproc+json" "$2" | jq '{
-    type: "article",
+    type,
     title,
-    volume,
+    volume: (.volume | tonumber),
     ISSN,
-    DOI,
-    number: (.issue // null),
+    DOI: ("https://doi.org/" + .DOI),
+    number: (.issue | tonumber // null),
     journal: ."container-title",
     publisher: (.publisher // ."publisher-name" // .publisher[0].name // null),
-    author: [.author[] | {family, given}],
+    author: [.author[] | "\(.family), \(.given)"],
     year: .issued."date-parts"[0][0],
     month: (.issued."date-parts"[0][1] | tonumber | . - 1 | ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"][.]),
     pages: (.page // null)
-}' | python3 -c 'import sys, yaml, json; print(yaml.dump(json.loads(sys.stdin.read())))'
+}' | python3 -c '
+import sys
+import yaml
+import json
+
+class OrderedLoader(yaml.SafeLoader):
+    pass
+
+def construct_mapping(loader, node):
+    loader.flatten_mapping(node)
+    return dict(loader.construct_pairs(node))
+
+OrderedLoader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping)
+json_data = json.loads(sys.stdin.read())
+print(yaml.dump(json_data, sort_keys=False, allow_unicode=True))
+'
 
 fi
